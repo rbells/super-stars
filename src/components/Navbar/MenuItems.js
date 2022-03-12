bec6957872bac298e7932a4307d88f40b46b493b
cgcrochet <80590239+cgcrochet@users.noreply.github.com>
2022-03-12 17:41:28 -0600

    Add files via upload

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Social Feed',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Recommendations',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
