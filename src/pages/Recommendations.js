e78b7db6d6313859d75f801ac5ccb4a4be99ab38
rbells <98972060+rbells@users.noreply.github.com>
2022-03-23 17:52:20 -0500

    added button

--- src/pages/Recommendations.js
+++ src/pages/Recommendations.js
@@ -2,11 +2,17 @@ import React from "react";
 import '../App.css';
 
 function Recommendations() {
+
+    const handleClick = () =>{
+        //nothing yet
+        console.log("Here you go");//testing that button works
+    }
     return(
     <div>
         <h1>
             Recommendations
         </h1>
+        <button onClick={handleClick}>Generate Recommendations</button>
     </div>
     )
 }
