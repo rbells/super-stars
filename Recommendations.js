ed85fb770ebc1eeda7223a08e88dcd53d8fbf8fc
Sphere39 <77363797+Sphere39@users.noreply.github.com>
2022-03-23 17:29:22 -0500

    Add files via upload

---
+++ Recommendations.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css';
+
+function Recommendations() {
+    return(
+    <div>
+        <h1>
+            Recommendations
+        </h1>
+        <body>
+            Add what kind of movies you would like.
+            
+        </body>
+    </div>
+    )
+}
+
+export default Recommendations;
