23149a5b7cc2af958269062c77380335da847ba4
rbells <98972060+rbells@users.noreply.github.com>
2022-03-23 17:17:12 -0500

    Add files via upload
    added profile link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Recommendations from "./components/Recommendations";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignUp from './components/SignUp';
 import RateMovies from './components/RateMovies';
+import Profile from './components/Profile';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/recommendations" element={<Recommendations />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/ratemovies" element={<RateMovies />} />
+          <Route path ="/profile" element={<Profile />} />
         </Routes>
       </div>
   );
