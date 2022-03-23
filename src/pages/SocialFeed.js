37a8a86a7f5d9f5f69162b4e1f545d305949ba3f
cgcrochet <80590239+cgcrochet@users.noreply.github.com>
2022-03-23 17:33:22 -0500

    Add files via upload

---
+++ src/pages/SocialFeed.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+
+function SocialFeed() {
+    return(
+    <div>
+        <h1>
+            Social Feed
+        </h1>
+    </div>
+    )
+}
+
+export default SocialFeed;
