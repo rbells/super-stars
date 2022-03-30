33f155ab0e4ff6d1a487d74ca758eb3bdc78a540
emilyvu66 <97489710+emilyvu66@users.noreply.github.com>
2022-03-29 19:58:07 -0500

    Add files via upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,34 @@
 import React from "react";
-import Follow from "../components/Follow";
 import '../App.css';
+import Sidebar from "../components/Sidebar/Sidebar";
+import Feed from "../components/Feed/Feed";
+import Rightbar from "../components/Rightbar/Rightbar";
+import './Profile.css';
+
 
 
 
 function Profile() {
     return(
-    <div>
-        <h1>
-            Profile
-        </h1>
-        <Follow />
-    </div>
-    
+        <div className="profile">
+            <Sidebar />
+             <div className="profileRight">
+                <div className="profileRightTop">
+                    <div className-="profileCover">
+                        <img className="profileCoverImg" Scr="assets/post/3.jpeg" alt=""/>
+                        <img className="profileUserImg" scr="assets/person/7.jpeg" alt=""/>
+                    </div>
+                    <div className="profileInfo">
+                        <h4 className="profileInfoName">Superstars</h4>
+                        <span className="profileInforDesc">Hello my friends</span>
+                    </div>
+                    </div>
+                <div className="profileRightBottom">
+                 <Feed />
+                 <Rightbar />
+                </div>
+            </div>
+        </div>
     )
 }
 
