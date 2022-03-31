a17571376933c3423eef215dd1f4ec5965f45861
FinnPhayerLSU <98072393+FinnPhayerLSU@users.noreply.github.com>
2022-03-30 17:08:03 -0700

    Add files via upload

---
+++ client/src/pages/Testing.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+import TestPostList from "../components/TestPostList";
+import Follow from "../components/Follow";
+import "../App.css";
+
+function Testing(){
+
+    const [userDetails,setUserDetails] = useState({
+        username:'',
+        userID:''
+      });
+
+    const[postDetails, setPostDetails] = useState([]);
+
+    Axios.defaults.withCredentials = true;
+
+    useEffect(()=>{
+        Axios.get("http://localhost:3001/api/login").then((response) =>{
+            if(response.data.loggedIn === true){
+                setUserDetails({...userDetails,username: response.data.user[0].username, 
+                                               userID: response.data.user[0].id});
+            }  
+        })
+    },[]);
+
+    const getPosts = (user) =>{
+        Axios.post("http://localhost:3001/api/getposts",{userID: user.userID}).then((response) =>{
+            setPostDetails(response.data);
+        })
+    }
+
+    useEffect(()=>{
+        getPosts(userDetails);
+    }, [userDetails])
+
+    return(
+        <div>
+            <Follow />
+            <h1>Posts</h1>
+            <button onClick={()=>(console.log(postDetails))}>Test</button>
+            <TestPostList posts={postDetails} />
+        </div>
+    )
+}
+
+export default Testing;
