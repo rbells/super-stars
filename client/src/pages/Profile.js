e6ef81cc15ef04036024a38a30b3e25d85135b81
emilyvu66 <97489710+emilyvu66@users.noreply.github.com>
2022-04-07 12:20:48 -0500

    Add files via upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,70 +1,34 @@
-import React,{useState, useEffect} from "react";
+import React from "react";
 import '../App.css';
 import Sidebar from "../components/Sidebar/Sidebar";
-import ProfileFeed from "../components/Feed/ProfileFeed";
+import Feed from "../components/Feed/Feed";
 import Rightbar from "../components/Rightbar/Rightbar";
 import './Profile.css';
-import Axios from 'axios';
+
+
 
 
 function Profile() {
-
-    const [userDetails,setUserDetails] = useState({
-        username:'',
-        userID:''
-      });
-
-    const[postDetails, setPostDetails] = useState([]);
-
-    const [friendsListID, setFriendsListID] = useState([]);
-
-    Axios.defaults.withCredentials = true;
-
-    useEffect(()=>{
-        Axios.get("http://localhost:3001/api/login").then((response) =>{
-            if(response.data.loggedIn === true){
-                setUserDetails({...userDetails,username: response.data.user[0].username, 
-                                               userID: response.data.user[0].id});
-            }  
-        })
-    },[]);
-
-    useEffect(()=>{
-        getFriendsList(userDetails)
-    },[userDetails])
-
-    const getFriendsList = (user) =>{
-        Axios.post("http://localhost:3001/api/getfriendslist",{userID: user.userID}).then((response) =>{
-            setFriendsListID(response.data);
-        })
-    }
-
-    const getPosts = (user) =>{
-        Axios.post("http://localhost:3001/api/getposts",{userID: user.userID}).then((response) =>{
-            setPostDetails(response.data);
-        })
-    }
-
-    useEffect(()=>{
-        getPosts(userDetails);
-    }, [userDetails])
-
     return(
         <div className="profile">
             <Sidebar />
              <div className="profileRight">
                 <div className="profileRightTop">
                     <div className-="profileCover">
-                        <img className="profileCoverImg" Scr="assets/post/3.jpeg" alt=""/>
-                        <img className="profileUserImg" scr="assets/person/7.jpeg" alt=""/>
+                        <img className="profileCoverImg" 
+                            scr="assets/post/3.jpeg" 
+                            alt=""/>
+                        <img className="profileUserImg" 
+                            scr="assets/profile.jpeg" 
+                            alt=""/>
                     </div>
                     <div className="profileInfo">
-                        <h4 className="profileInfoName">Superstars</h4>
-                        <span className="profileInforDesc">Hello my friends</span>
+                        <h4 className="profileInfoName">Team Superstars</h4>
+                        <span className="profileInfoDesc">I love computer science.</span>
                     </div>
                     </div>
                 <div className="profileRightBottom">
-                 <ProfileFeed user={userDetails} posts={postDetails}/>
+                 <Feed />
                  <Rightbar />
                 </div>
             </div>
