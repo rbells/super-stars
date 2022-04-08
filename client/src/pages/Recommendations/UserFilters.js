417a45b829020da388ea6fd2c96467b0c7e50da1
emilyvu66 <97489710+emilyvu66@users.noreply.github.com>
2022-04-07 21:22:05 -0500

    Add files via upload

--- client/src/pages/Recommendations/UserFilters.js
+++ client/src/pages/Recommendations/UserFilters.js
@@ -29,9 +29,9 @@ function UserFilters(){
         return(
         
             <div>
-                <h1>User Filters</h1>
-                <h2>Genre</h2>
-                <div>
+                <h1 className="userFilter">User Filters</h1>
+                <h2 className="genre">Genre</h2>
+                <div className="genreButton"> 
                     <Selection addToSelections={addToSelections} title="Comedy"/>
                     <Selection addToSelections={addToSelections} title="Drama"/>
                     <Selection addToSelections={addToSelections} title="Horror"/>
@@ -39,8 +39,8 @@ function UserFilters(){
                     <Selection addToSelections={addToSelections} title="Action"/>
                     <Selection addToSelections={addToSelections} title="Family"/>
                 </div>
-                <h2>Decade</h2>
-                <div>
+                <h2 className="decade">Decade</h2>
+                <div className="titleButton">
                     <Selection addToSelections={addToSelections} title="1940's"/>
                     <Selection addToSelections={addToSelections} title="1950's"/>
                     <Selection addToSelections={addToSelections} title="1960's"/>
@@ -51,8 +51,8 @@ function UserFilters(){
                     <Selection addToSelections={addToSelections} title="2010's"/>
                     <Selection addToSelections={addToSelections} title="2020's"/>
                 </div>
-                <h2>Finished</h2>
-                <div>
+                <h2 className="finished">Finished</h2>
+                <div className="giveButton">
                     <button onClick={handleFinalClick}>Give Me My Movie!</button>
                 </div>
             </div>
